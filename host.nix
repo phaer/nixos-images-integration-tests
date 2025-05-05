7fb2f233e0330d83811529e0033f0ebb3ed6ca25
phaer <hello@phaer.org>
2025-05-05 19:33:55 +0200

    make disk larger, closure a bit smaller

--- host.nix
+++ host.nix
@@ -1,6 +1,8 @@
-{
+{modulesPath, ...}: {
+  imports = [ "${modulesPath}/profiles/minimal.nix" ];
   config = {
     system.stateVersion = "25.05";
+    virtualisation.diskSize = 1024 * 2;
 
     image.modules.repart-efi-gpt =
       {...}: {
