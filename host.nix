1ff0a2c7843789426eab0d6be055326d207f3047
phaer <hello@phaer.org>
2025-05-05 20:27:08 +0200

    host:  amazon image overrides

--- host.nix
+++ host.nix
@@ -1,8 +1,16 @@
-{modulesPath, ...}: {
+{lib, modulesPath, ...}: {
   imports = [ "${modulesPath}/profiles/minimal.nix" ];
   config = {
     system.stateVersion = "25.05";
     virtualisation.diskSize = 1024 * 2;
+
+    # image setting collides with
+    # nixpkgs/nixos/lib/testing/network.nix during testing
+    image.modules.amazon.config = {
+      ec2.efi = true;
+      networking.hostName = lib.mkForce "";
+      systemd.services."serial-getty@ttyS0".enable = lib.mkForce true;
+    };
 
     image.modules.repart-efi-gpt =
       {...}: {
