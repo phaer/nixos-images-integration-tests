af796ce31a82ca0b31835e0a5b318f91de508e02
phaer <hello@phaer.org>
2025-05-23 11:19:38 +0200

    host: use mkForce with autologinUser...
    to overwrite the default in iso-installer

--- host.nix
+++ host.nix
@@ -4,7 +4,8 @@
     system.stateVersion = "25.05";
     virtualisation.diskSize = 1024 * 4;
     boot.kernelParams = [ "console=ttyS0,115200n8" ];
-    services.getty.autologinUser = "root";
+
+    services.getty.autologinUser = lib.mkForce "root";
 
     # FIXME: cloud-init can't reach the metadata api and therefore keeps
     # systemd in "starting" state. To include it, it we need to run our
