93d03ada415aac5266602aaa10f726d27c90d2cc
phaer <hello@phaer.org>
2025-05-23 11:16:58 +0200

    host: remove image overrides...
    which were useful back when we were trying to use  VM tests

--- host.nix
+++ host.nix
@@ -1,21 +1,10 @@
-{lib, modulesPath, ...}: {
+{modulesPath, ...}: {
   imports = [ "${modulesPath}/profiles/minimal.nix" ];
   config = {
     system.stateVersion = "25.05";
     virtualisation.diskSize = 1024 * 4;
     boot.kernelParams = [ "console=ttyS0,115200n8" ];
     services.getty.autologinUser = "root";
-
-    # image setting collides with nixpkgs/nixos/lib/testing/network.nix during testing
-    image.modules.amazon.config = {
-      ec2.efi = true;
-      networking.hostName = lib.mkForce "";
-    };
-
-    # image setting collides with nixpkgs/nixos/lib/testing/network.nix during testing
-    image.modules.digital-ocean.config = {
-      networking.hostName = lib.mkForce "";
-    };
 
     image.modules.repart-efi-gpt =
       {...}: {
