6989f94d379968ab53510db03923dc82f05ae75d
phaer <hello@phaer.org>
2025-05-23 11:18:42 +0200

    host: disable cloud-init for now

--- host.nix
+++ host.nix
@@ -1,10 +1,15 @@
-{modulesPath, ...}: {
+{lib, modulesPath, ...}: {
   imports = [ "${modulesPath}/profiles/minimal.nix" ];
   config = {
     system.stateVersion = "25.05";
     virtualisation.diskSize = 1024 * 4;
     boot.kernelParams = [ "console=ttyS0,115200n8" ];
     services.getty.autologinUser = "root";
+
+    # FIXME: cloud-init can't reach the metadata api and therefore keeps
+    # systemd in "starting" state. To include it, it we need to run our
+    # own cloud-init server for the VM.
+    services.cloud-init.enable = lib.mkForce false;
 
     image.modules.repart-efi-gpt =
       {...}: {
