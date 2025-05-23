aca235c1946cf2da3498b55a198243b682531257
phaer <hello@phaer.org>
2025-05-23 15:42:59 +0200

    add check_boot_all script for local checks...
    ...and a commented ignore list

---
+++ ignore.nix
@@ -0,0 +1,15 @@
+[
+  "google-compute"  # FIXME: eval error, google-guest-configs pkg "not valid". 4 years out of date upstream
+  "lxc-metadata" # compressed container tarball
+  "proxmox-lxc" # compressed container tarball
+  "lxc" # compressed container tarball
+  "iso" # FIXME: iso, "no bootable device"
+  "iso-installer" # FIXME: iso,  hangs at "virtual console setup"
+  "kexec" # compressed kexec tarball
+  "proxmox"  # FIXME failing build, vm crashes during vma creating
+  "sd-card" # FIXME: compression
+  "linode" # FIXME: compression
+  "digital-ocean" # FIXME: compression
+  "vagrant-virtualbox" # FIXME: ova, single file output
+  "virtualbox" # FIXME: ova
+]
