9697afcf8c3b2bc1ee52fb777c3187417fe5e6e9
phaer <hello@phaer.org>
2025-05-27 16:20:10 +0200

    ignore openstack(-zfs) for now

--- ignore.nix
+++ ignore.nix
@@ -12,4 +12,6 @@
   "digital-ocean" # FIXME: compression
   "vagrant-virtualbox" # FIXME: ova, single file output
   "virtualbox" # FIXME: ova
+  "openstack" # FIXME: reaches multi-user-target but no getty on serial
+  "openstack-zfs" # FIXME: reaches multi-user-target but no getty on serial
 ]
