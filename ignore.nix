381b2c77e93a5f0bc1f4a806395fd865504b16fe
phaer <hello@phaer.org>
2025-07-20 14:45:55 +0200

    ignore: add sd-card

--- ignore.nix
+++ ignore.nix
@@ -9,6 +9,7 @@
   "proxmox"  # FIXME failing build, vm crashes during vma creating
   "vagrant-virtualbox" # FIXME: ova, single file output
   "virtualbox" # FIXME: ova
+  "sd-card" # FIXME: uses generic-extlinux bootloader. Maybe do direct boot to test the fs at least?
   "linode" # FIXME: linode uses direct boot, see e.g. https://www.linode.com/community/questions/24318/how-exactly-does-linode-boot-a-disk
   "openstack" # FIXME: reaches multi-user-target but no getty on serial
   "openstack-zfs" # FIXME: reaches multi-user-target but no getty on serial
